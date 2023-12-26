d354fd2680dfe04065463570d92a777d65e45ddf
Wallace McGee <wm@wolispace.com>
2023-12-26 14:37:57 +1030

    add NODE_ENV

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const http = require("http");
 
+const nodeEnv = process.env.NODE_ENV;
+
 const host = '0.0.0.0';
 const showHost = 'localhost';
 const port = 8080;
 
 const requestListener = function (req, res) {
     res.writeHead(200);
-    res.end(`Hello world! from http://${showHost}:${port}`);
+    res.end(`Hello world! from [${nodeEnv}] http://${showHost}:${port}`);
 };
 
 const server = http.createServer(requestListener);
 server.listen(port, host, () => {
-    console.log(`Server is running on http://${showHost}:${port}`);
+    console.log(`Server is running on [${nodeEnv}] http://${showHost}:${port}`);
 });
--- start_server.bat
+++ start_server.bat
@@ -1,3 +1,3 @@
+set NODE_ENV=dev
 docker compose up -d
-
 start "" http://localhost:8080
